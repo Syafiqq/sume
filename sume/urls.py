29153b5f10a23ee185f4e4df0b13caa0e25cada1
Daneswara Jauhari <daneswarajauhari@gmail.com>
2018-07-07 06:55:16 +0700

    static

--- sume/urls.py
+++ sume/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include,path
+from django.urls import include,path, re_path
+from django.conf import settings
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += re_path('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
