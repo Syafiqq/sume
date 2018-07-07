f1bf50822d3dd2cc1a2545eb0c2714c8e5185ac4
Daneswara Jauhari <daneswarajauhari@gmail.com>
2018-07-07 07:28:44 +0700

    tambah module

--- sume/settings/production.py
+++ sume/settings/production.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'sume.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=500, ssl_require=True)
+    'default': dj_database_url.config(conn_max_age=500, require_ssl=True)
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
