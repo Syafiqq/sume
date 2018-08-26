9f329f4e96a9e496e04be66dd7a55b3d4f1e3ef0
Syafiq <syafiq.rezpector@gmail.com>
2018-08-26 16:43:22 +0700

    Add forgot route

--- app/urls.py
+++ app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
 
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
+    path('forgot', views.forgot, name='forgot'),
     path('user', views.user, name='user'),
     path('kelas', views.kelas, name='kelas'),
     path('admin', views.admin, name='admin'),
