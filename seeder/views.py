fe6abff0e25fe2f8157961dbaade6f84ff372f92
Syafiq <syafiq.rezpector@gmail.com>
2018-09-05 23:40:13 +0700

    Add index controller

--- seeder/views.py
+++ seeder/views.py
@@ -1,1 +1,14 @@
 # Create your views here.
+import datetime
+
+from django.http import HttpResponse
+
+from seeder.database.seeder import UserSeeder
+
+
+def index(request):
+    UserSeeder.seed()
+
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
