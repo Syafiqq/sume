b940cc8effd2dcd92411ab6c191efdb76aa4d625
Syafiq <syafiq.rezpector@gmail.com>
2018-09-05 23:39:23 +0700

    Wrap logic to seed function

--- seeder/database/seeder/UserSeeder.py
+++ seeder/database/seeder/UserSeeder.py
@@ -2,20 +2,22 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from django_seed import Seed
 
-seeder = Seed.seeder()
 
-seeder.add_entity(User, 1, {
-    'username': lambda x: 'root',
-    'email': lambda x: 'root@mail.com',
-    'password': lambda x: make_password('secret'),
-    'is_superuser': lambda x: True
-})
-seeder.execute()
+def seed():
+    seeder = Seed.seeder()
 
-seeder.add_entity(User, 5, {
-    'username': lambda x: seeder.faker.name(),
-    'email': lambda x: seeder.faker.safe_email(),
-    'password': lambda x: make_password('secret'),
-    'is_superuser': lambda x: False
-})
-seeder.execute()
+    seeder.add_entity(User, 1, {
+        'username': lambda x: 'root',
+        'email': lambda x: 'root@mail.com',
+        'password': lambda x: make_password('secret'),
+        'is_superuser': lambda x: True
+    })
+    seeder.execute()
+
+    seeder.add_entity(User, 5, {
+        'username': lambda x: seeder.faker.name(),
+        'email': lambda x: seeder.faker.safe_email(),
+        'password': lambda x: make_password('secret'),
+        'is_superuser': lambda x: False
+    })
+    seeder.execute()
