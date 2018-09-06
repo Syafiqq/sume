f13f430da8f0d1b309ba2c0b0e7437ac7da5e6a8
Syafiq <syafiq.rezpector@gmail.com>
2018-09-06 22:45:38 +0700

    separate seeder and data

--- seeder/database/seeder/GroupSeeder.py
+++ seeder/database/seeder/GroupSeeder.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import Group
 
+groups = ['root', 'student', 'organization']
+
 
 def seed():
-    for group in ['root', 'student', 'organization']:
+    for group in groups:
         if not Group.objects.filter(name=group).exists():
             Group.objects.create(name=group)
