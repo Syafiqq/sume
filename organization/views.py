4ad7c2646fdbe241df85b5e5678d55e52d031d9c
Syafiq <syafiq.rezpector@gmail.com>
2018-09-12 22:45:47 +0700

    Add login required

--- organization/views.py
+++ organization/views.py
@@ -1,7 +1,10 @@
 # Create your views here.
 from django.http import HttpResponse
 
+from app.app.utils.custom.decorators import login_required
 
+
+@login_required(login_url='/login')
 def index(request):
     html = "<html><body>Index of Organization</body></html>"
     return HttpResponse(html)
