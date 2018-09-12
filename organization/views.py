6a24f2085d5421cbc758405c4658899b8078d617
Syafiq <syafiq.rezpector@gmail.com>
2018-09-13 00:03:51 +0700

    Add basic view

--- organization/views.py
+++ organization/views.py
@@ -1,10 +1,16 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render
 
+from app.app.utils.arrayutil import array_merge
+from app.app.utils.commonutil import initialize_form_context, fetch_message
 from app.app.utils.custom.decorators import login_required
 
 
 @login_required(login_url='/login')
 def index(request):
-    html = "<html><body>Index of Organization</body></html>"
-    return HttpResponse(html)
+    context = array_merge(initialize_form_context(), fetch_message(request))
+
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, '_root.html', context)
