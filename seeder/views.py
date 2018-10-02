179a7621d0a7808124f8d0d7af09cec6214a35a1
Syafiq <syafiq.rezpector@gmail.com>
2018-10-02 20:58:53 +0700

    Implement Class Seeder

--- seeder/views.py
+++ seeder/views.py
@@ -3,17 +3,19 @@ import datetime
 
 from django.http import HttpResponse
 
-from seeder.database.seeder import UserSeeder, GroupSeeder, DocumentSeeder
+from seeder.database.seeder import UserSeeder, GroupSeeder, DocumentSeeder, ClassSeeder
 
 
 def index(request):
     UserSeeder.truncate()
     GroupSeeder.truncate()
     DocumentSeeder.truncate()
+    ClassSeeder.truncate()
 
     GroupSeeder.seed()
     UserSeeder.seed()
     DocumentSeeder.seed()
+    ClassSeeder.seed()
 
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
