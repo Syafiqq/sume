20341fafa99cf70ec69e8425bd42276e4c4f7d9a
Syafiq <syafiq.rezpector@gmail.com>
2018-10-02 20:41:08 +0700

    Implement document seeder

--- seeder/views.py
+++ seeder/views.py
@@ -3,15 +3,17 @@ import datetime
 
 from django.http import HttpResponse
 
-from seeder.database.seeder import UserSeeder, GroupSeeder
+from seeder.database.seeder import UserSeeder, GroupSeeder, DocumentSeeder
 
 
 def index(request):
     UserSeeder.truncate()
     GroupSeeder.truncate()
+    DocumentSeeder.truncate()
 
     GroupSeeder.seed()
     UserSeeder.seed()
+    DocumentSeeder.seed()
 
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
