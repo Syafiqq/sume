94ac111f80c75308053142b7e2be9ec77b6ca399
Syafiq <syafiq.rezpector@gmail.com>
2018-10-09 07:27:28 +0700

    Fix seeder filename

--- seeder/database/seeder/DocumentSeeder.py
+++ seeder/database/seeder/DocumentSeeder.py
@@ -7,12 +7,12 @@ from app.models import Dokumen
 
 seeds = {
     'root@mail.com': [
-        Dokumen(nama_file='Dokumen1', pub_date=datetime.date(2018, 10, 2), filenya='dokumen/2018/10/02/Dokumen1.pdf'),
-        Dokumen(nama_file='Dokumen2', pub_date=datetime.date(2018, 10, 2), filenya='dokumen/2018/10/02/Dokumen2.pdf'),
-        Dokumen(nama_file='Dokumen3', pub_date=datetime.date(2018, 10, 2), filenya='dokumen/2018/10/02/Dokumen3.pdf'),
-        Dokumen(nama_file='Dokumen4', pub_date=datetime.date(2018, 10, 2), filenya='dokumen/2018/10/02/Dokumen4.pdf'),
-        Dokumen(nama_file='Dokumen5', pub_date=datetime.date(2018, 10, 2), filenya='dokumen/2018/10/02/Dokumen5.pdf'),
-        Dokumen(nama_file='Dokumen6', pub_date=datetime.date(2018, 10, 2), filenya='dokumen/2018/10/02/Dokumen6.pdf'),
+        Dokumen(nama_file='Document1', pub_date=datetime.date(2018, 10, 2), filenya='dokumen/2018/10/02/Dokumen1.pdf'),
+        Dokumen(nama_file='Document2', pub_date=datetime.date(2018, 10, 2), filenya='dokumen/2018/10/02/Dokumen2.pdf'),
+        Dokumen(nama_file='Document3', pub_date=datetime.date(2018, 10, 2), filenya='dokumen/2018/10/02/Dokumen3.pdf'),
+        Dokumen(nama_file='Document4', pub_date=datetime.date(2018, 10, 2), filenya='dokumen/2018/10/02/Dokumen4.pdf'),
+        Dokumen(nama_file='Document5', pub_date=datetime.date(2018, 10, 2), filenya='dokumen/2018/10/02/Dokumen5.pdf'),
+        Dokumen(nama_file='Document6', pub_date=datetime.date(2018, 10, 2), filenya='dokumen/2018/10/02/Dokumen6.pdf'),
     ]
 }
 
