457d3206363788086ac32a435f0485a116654006
Syafiq <syafiq.rezpector@gmail.com>
2018-10-11 06:36:44 +0700

    Fix DOcument name

--- seeder/database/seeder/DocumentSeeder.py
+++ seeder/database/seeder/DocumentSeeder.py
@@ -6,17 +6,17 @@ from app.models import Dokumen
 
 documents = [
     Dokumen(id=1, nama_file='Document1', user_id=1, pub_date=datetime.date(2018, 10, 2),
-            filenya='dokumen/2018/10/02/Dokumen1.pdf'),
+            filenya='dokumen/2018/10/02/Document1.pdf'),
     Dokumen(id=2, nama_file='Document2', user_id=1, pub_date=datetime.date(2018, 10, 2),
-            filenya='dokumen/2018/10/02/Dokumen2.pdf'),
+            filenya='dokumen/2018/10/02/Document2.pdf'),
     Dokumen(id=3, nama_file='Document3', user_id=1, pub_date=datetime.date(2018, 10, 2),
-            filenya='dokumen/2018/10/02/Dokumen3.pdf'),
+            filenya='dokumen/2018/10/02/Document3.pdf'),
     Dokumen(id=4, nama_file='Document4', user_id=1, pub_date=datetime.date(2018, 10, 2),
-            filenya='dokumen/2018/10/02/Dokumen4.pdf'),
+            filenya='dokumen/2018/10/02/Document4.pdf'),
     Dokumen(id=5, nama_file='Document5', user_id=1, pub_date=datetime.date(2018, 10, 2),
-            filenya='dokumen/2018/10/02/Dokumen5.pdf'),
+            filenya='dokumen/2018/10/02/Document5.pdf'),
     Dokumen(id=6, nama_file='Document6', user_id=1, pub_date=datetime.date(2018, 10, 2),
-            filenya='dokumen/2018/10/02/Dokumen6.pdf'),
+            filenya='dokumen/2018/10/02/Document6.pdf'),
 ]
 
 
