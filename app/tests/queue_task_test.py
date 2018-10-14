1c8d682c9cf4e8bee2093c98e1896f93a2431936
Syafiq <syafiq.rezpector@gmail.com>
2018-10-14 18:29:22 +0700

    Add queue task test

---
+++ app/tests/queue_task_test.py
@@ -0,0 +1,21 @@
+from app.models import Dokumen
+from app.tasks import simulate_sleep, proceed_document
+from app.tests.testcase import OTestCase
+
+
+class QueueTaskTest(OTestCase):
+    def test_sleep_main_thread(self):
+        self.assertIsNone(None)
+        simulate_sleep(5)
+
+    def test_sleep_background_thread(self):
+        self.assertIsNone(None)
+        simulate_sleep.delay(5)
+
+    def test_dlnn_main_thread(self):
+        self.assertIsNone(None)
+        proceed_document(Dokumen())
+
+    def test_dlnn_background_thread(self):
+        self.assertIsNone(None)
+        proceed_document.delay(Dokumen())
