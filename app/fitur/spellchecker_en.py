1dd2313fec7832ffc315af0c05c73dd23b7683ab
daneswara <daneswarajauhari@gmail.com>
2018-10-16 05:12:03 +0700

    add delay to method

--- app/fitur/spellchecker_en.py
+++ app/fitur/spellchecker_en.py
@@ -1,9 +1,11 @@
 from spellchecker import SpellChecker
 
-spell = SpellChecker()
+spell = SpellChecker(distance=1)
+spell.word_frequency.load_text_file('/home/daneswara/Django/app/fitur/id_full.txt')
+spell.word_frequency.lo
 
 # find those words that may be misspelled
-misspelled = spell.unknown(['something', 'tdak', 'hapenning', 'bsa'])
+misspelled = spell.unknown(['salahs', 'danes', 'apa', 'kok', 'iiya', 'bagaaimana'])
 
 for word in misspelled:
     # Get the one `most likely` answer
