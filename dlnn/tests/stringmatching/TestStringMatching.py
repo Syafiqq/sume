1b439a8a40e17881e110a60070e33787b0541efc
Syafiq <syafiq.rezpector@gmail.com>
2018-10-25 23:19:21 +0700

    Add Test String matching

--- dlnn/tests/stringmatching/TestStringMatching.py
+++ dlnn/tests/stringmatching/TestStringMatching.py
@@ -3,6 +3,8 @@ import re
 import numpy as np
 import pandas as pd
 import sparse_dot_topn.sparse_dot_topn as ct
+from gensim.models.doc2vec import TaggedDocument, Doc2Vec
+from nltk import word_tokenize
 from scipy.sparse import csr_matrix
 
 from dlnn.tests.ml.testcase import TestCase
@@ -127,3 +129,79 @@ box."""
         print("SELFTIMED:", t)
         matches_df = get_matches_df(matches, d1tt)
         print(matches_df.sort_values(['similairity'], ascending=False))
+
+    def test_doc_2_vec_train(self):
+        d1 = """With Django, you can take Web applications from concept to launch in a matter
+    of hours. Django takes care of much of the hassle of Web development, so you
+    can focus on writing your app without needing to reinvent the wheel. It’s free
+    and open source."""
+
+        d1t = tokenize(d1)
+        d1tt = make_words(d1t, 3)
+        tagged_data = [TaggedDocument(words=word_tokenize(_d.lower()), tags=[str(i)]) for i, _d in enumerate(d1tt)]
+        max_epochs = 1000
+        vec_size = 200
+        alpha = 0.025
+        model = Doc2Vec(size=vec_size,
+                        alpha=alpha,
+                        min_alpha=0.00025,
+                        min_count=1,
+                        dbow_words=1,
+                        dm=1)
+        model.build_vocab(tagged_data)
+        for epoch in range(max_epochs):
+            print('iteration {0}'.format(epoch))
+            model.train(tagged_data,
+                        total_examples=model.corpus_count,
+                        epochs=model.iter)
+            model.alpha -= 0.0002
+            model.min_alpha = model.alpha
+        model.save("d2v.model")
+
+    def test_doc_2_vec_test(self):
+        d1 = """With Django, you can take Web applications from concept to launch in a matter
+        of hours. Django takes care of much of the hassle of Web development, so you
+        can focus on writing your app without needing to reinvent the wheel. It’s free
+        and open source."""
+
+        d2 = """Django includes dozens of extras you can use to handle common Web
+        development tasks. Django takes care of user authentication, content
+        administration, site maps, RSS feeds, and many more tasks — right out of the
+        box. Django includes dozens of extras you can use to handle common Web
+development tasks. Django takes care of user authentication, content
+administration, site maps, RSS feeds, and many more tasks — right out of the
+box."""
+
+        d1t = tokenize(d1)
+        d2t = tokenize(d2)
+
+        d1tt = make_words(d1t, 3)
+        d2tt = make_words(d2t, 3)
+
+        model = Doc2Vec.load("d2v.model")
+        # to find the vector of a document which is not in training data
+        # test_data = word_tokenize(d1tt[0].lower())
+        # v1 = model.infer_vector(test_data)
+        # print("V1_infer", v1)
+
+        # tagged_data = [TaggedDocument(words=word_tokenize(_d.lower()), tags=[str(i)]) for i, _d in enumerate(d1tt)]
+        # for tag in tagged_data:
+        #    print(tag)
+        #    v1 = model.infer_vector(tagged_data)
+        # print("V1_infer", v1)
+
+        # to find most similar doc using tags
+        # similar_doc = model.docvecs.most_similar([model.infer_vector(test_data)])
+
+        # test_data = word_tokenize(d1tt[0].lower())
+        # similar_doc = model.docvecs.most_similar(positive=[model.infer_vector('testing'.split(' '))])
+        # print(list(filter(lambda x: x[1], similar_doc)))
+
+        # to find vector of doc in training data using tags or in other words, printing the vector of document at index 1 in training data
+        # print(model.docvecs['1'])
+        count = 0
+        for x in d2tt:
+            sim = model.docvecs.most_similar(positive=[model.infer_vector(word_tokenize(x.lower()))])
+            xy = max(sim, key=lambda z: z[1])
+            count += 1 if xy[1] > 0.5 else 0
+        print('%d of %d' % (count, len(d2tt)))
